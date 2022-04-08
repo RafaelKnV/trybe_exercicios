616c225b621ff0c2733958ea5c9b3cf71d3d3e40
Rafael Vitor <rafaelvitor@itaoca.net>
2022-04-08 15:46:07 -0300

    Exercício 5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercício 5/Ex.js
@@ -0,0 +1,15 @@
+const a = 34
+const b = 46
+const c = 100
+let somaAngulos = (a + b + c)
+if(a > 0 && b > 0 && c > 0){
+    if(somaAngulos === 180){
+        console.log("true")
+    }
+    else{
+        console.log("false");
+    }
+}
+else{
+    console.log("error");
+}
